2a4fdbac53df89d4110932463d947b01b3107ac6
DigvijayWagh22 <126435074+DigvijayWagh22@users.noreply.github.com>
2024-01-21 09:44:59 +0530

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -7,7 +7,9 @@ import (
 )
 
 func main() {
+	// Run the server and handle any potential errors
 	if err := server.Run(); err != nil {
+		// Log a fatal error and exit if the server fails to start
 		log.Fatalln(err.Error())
 	}
 }
