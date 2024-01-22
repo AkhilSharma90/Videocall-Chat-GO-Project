f874f2077a02ae106c80c9c06867936c71d75148
DigvijayWagh22 <126435074+DigvijayWagh22@users.noreply.github.com>
2024-01-22 09:45:31 +0530

    Update chat.go

--- internal/handlers/chat.go
+++ internal/handlers/chat.go
@@ -8,10 +8,12 @@ import (
 	"github.com/gofiber/websocket/v2"
 )
 
+// RoomChat handles the HTTP request to render the chat page for a room.
 func RoomChat(c *fiber.Ctx) error {
 	return c.Render("chat", fiber.Map{}, "layouts/main")
 }
 
+// RoomChatWebsocket handles WebSocket connections for the chat in a room.
 func RoomChatWebsocket(c *websocket.Conn) {
 	uuid := c.Params("uuid")
 	if uuid == "" {
@@ -27,9 +29,11 @@ func RoomChatWebsocket(c *websocket.Conn) {
 	if room.Hub == nil {
 		return
 	}
+	// Establish WebSocket connection for chat between peers in the room.
 	chat.PeerChatConn(c.Conn, room.Hub)
 }
 
+// StreamChatWebsocket handles WebSocket connections for the chat in a streaming session.
 func StreamChatWebsocket(c *websocket.Conn) {
 	suuid := c.Params("suuid")
 	if suuid == "" {
@@ -44,6 +48,7 @@ func StreamChatWebsocket(c *websocket.Conn) {
 			stream.Hub = hub
 			go hub.Run()
 		}
+		// Establish WebSocket connection for chat between peers in the streaming session.
 		chat.PeerChatConn(c.Conn, stream.Hub)
 		return
 	}
